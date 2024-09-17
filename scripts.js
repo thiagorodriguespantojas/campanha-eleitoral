19b11b4468edbb7034fd5a731d104a6a2e8f1c08
Thiago Rodrigues Pantoja <thiago.pantoja@easynext.com.br>
2024-09-17 16:00:23 -0300

    Atualize a seção de biografia e propostas
    - Revisei e atualizei o conteúdo da biografia para refletir as informações mais recentes sobre o Prefeito Fonseca 10 e o Vice-Prefeito Quinho Azevedo.
    - Adicionei novas propostas de governo que foram recentemente acordadas pela equipe de campanha.

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// Função simples para mostrar uma mensagem
+function showMessage() {
+    alert("Obrigado por apoiar a campanha de Fonseca 10 e Quinho Azevedo! Juntos vamos transformar nossa cidade!");
+}
