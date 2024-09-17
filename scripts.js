b803cafe5f1850c17f2479ff91a1c21a17a76a5d
Thiago Rodrigues Pantoja <thiago.pantoja@easynext.com.br>
2024-09-17 16:01:44 -0300

    Adicione novas imagens à galeria e melhore a responsividade
    - Incluí novas imagens na galeria para destacar eventos recentes da campanha.
    - Ajustei o CSS para melhorar a responsividade da galeria em dispositivos móveis, garantindo que as imagens se ajustem corretamente em todas as telas

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-// Função simples para mostrar uma mensagem
-function showMessage() {
-    alert("Obrigado por apoiar a campanha de Fonseca 10 e Quinho Azevedo! Juntos vamos transformar nossa cidade!");
+function alertMessage() {
+    alert('Obrigado por apoiar a campanha de Fonseca 10 e Quinho Azevedo!');
 }
